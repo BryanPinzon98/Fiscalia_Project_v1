fe309e5288ad840eaa9d4b4744669837cee9b64a
Bryan <bryans.pinzonc@gmail.com>
2020-02-19 11:43:04 -0600

    Project exported from IntelliJ to Netbeans

---
+++ Fiscalia_Project_GUI/src/gov/fged/enrollment/Enrollment.java
@@ -0,0 +1,47 @@
+package gov.fged.enrollment;
+
+import com.digitalpersona.onetouch.*;
+import com.digitalpersona.onetouch.processing.DPFPEnrollment;
+import com.digitalpersona.onetouch.processing.DPFPImageQualityException;
+import gov.fged.resources.LaunchFingerprintReader;
+import gov.fged.storage.Storage;
+import gov.fged.main.MainClass;
+
+
+public class Enrollment extends LaunchFingerprintReader {
+
+    private DPFPEnrollment enroller = DPFPGlobal.getEnrollmentFactory().createEnrollment();
+
+    public Enrollment() {
+        super();
+    }
+
+    @Override
+    public void process(DPFPSample sample) {
+        super.process(sample);
+
+        DPFPFeatureSet features = super.extractFeatures(sample, DPFPDataPurpose.DATA_PURPOSE_ENROLLMENT);
+
+        if (features != null) {
+            try {
+                enroller.addFeatures(features);
+            } catch (DPFPImageQualityException e) {
+
+            } finally {
+
+                switch (enroller.getTemplateStatus()) {
+                    case TEMPLATE_STATUS_READY:
+                        stop();
+                        MainClass mainClass = MainClass.getInstance();
+                        mainClass.setTemplate(enroller.getTemplate());
+
+                        Storage storage = new Storage();
+                        break;
+                    case TEMPLATE_STATUS_FAILED:
+                        stop();
+                        break;
+                }
+            }
+        }
+    }
+}
---
+++ Fiscalia_Project_GUI/src/gov/fged/storage/Storage.java
@@ -0,0 +1,54 @@
+package gov.fged.storage;
+
+import gov.fged.main.MainClass;
+import gov.fged.resources.TemplateFileFilter;
+
+import javax.swing.*;
+import java.io.File;
+import java.io.FileOutputStream;
+
+public class Storage extends JFrame {
+
+
+    public Storage() {
+        onSave();
+    }
+
+    private void onSave() {
+
+        JFileChooser chooser = new JFileChooser();
+        chooser.addChoosableFileFilter(new TemplateFileFilter());
+
+        while (true) {
+
+            if (chooser.showSaveDialog(this) == JFileChooser.APPROVE_OPTION) {
+                try {
+                    File file = chooser.getSelectedFile();
+                    if (!file.toString().toLowerCase().endsWith(".fpt")) {
+                        file = new File(file.toString() + ".fpt");
+                    }
+                    if (file.exists()) {
+                        int choice = JOptionPane.showConfirmDialog(this,
+                                String.format("File \"%1$s\" already exists. \nDo you want to replace it?", file.toString()),
+                                "Fingerprint saving", JOptionPane.YES_NO_CANCEL_OPTION);
+                        if (choice == JOptionPane.NO_OPTION) {
+                            continue;
+                        } else if (choice == JOptionPane.CANCEL_OPTION) {
+                            break;
+                        }
+                    }
+
+                    MainClass mainClass = MainClass.getInstance();
+
+                    FileOutputStream stream = new FileOutputStream(file);
+                    stream.write(mainClass.getTemplate().serialize());
+                    stream.close();
+
+                } catch (Exception e) {
+                    JOptionPane.showMessageDialog(this, e.getLocalizedMessage(), "Fingerprint saving", JOptionPane.ERROR_MESSAGE);
+                }
+            }
+            break;
+        }
+    }
+}
